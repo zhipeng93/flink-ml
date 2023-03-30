780d3a3618186a050e6c743aedd43efc5263399f
congzhou.zzp <zhangzhipengw@gmail.com>
2023-03-30 10:03:59 +0800

    remove model id in ServerNode for now to simplify the coding

--- flink-ml-lib/src/main/java/org/apache/flink/ml/common/optimizer/ps/ServerNode.java
+++ flink-ml-lib/src/main/java/org/apache/flink/ml/common/optimizer/ps/ServerNode.java
@@ -19,15 +19,10 @@ import org.apache.flink.util.Preconditions;
 
 import it.unimi.dsi.fastutil.longs.Long2DoubleOpenHashMap;
 
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 /** The server node that maintains the model parameters. */
 public class ServerNode extends AbstractStreamOperator<Tuple2<Integer, byte[]>>
@@ -42,26 +37,29 @@ public class ServerNode extends AbstractStreamOperator<Tuple2<Integer, byte[]>>
     private final double lambda2;
 
     // TODO: make it more general for all servers, not only FTRL.
-    private final Map<Integer, ServerVector> modelData;
+    private ServerVector modelData;
     private double[] sigma;
     private double[] z;
     private double[] n;
 
     private int psId = -1;
 
+    // private final int numServerCores = 1;
+
+    // final SerializableObject lock = new SerializableObject();
+    // for possible speed up.
+    // private transient ExecutorService fixedThreadPool;
+
+    // List<Future<?>> futuresInEpoch = new ArrayList<>();
+
     // Only useful in sync mode.
     private final boolean sync;
-    private final Map<Integer, Long2DoubleOpenHashMap> accumulatedGradByModelId = new HashMap<>();
-    private final Map<Integer, Double> weightByModelId = new HashMap<>();
-    private final Map<Integer, List<byte[]>> pendingPullsByModelId = new HashMap<>();
 
-    private int numServerCores = 1;
-
-    final SerializableObject lock = new SerializableObject();
-    // for possible speed up.
-    private transient ExecutorService fixedThreadPool;
-
-    List<Future<?>> futuresInEpoch = new ArrayList<>();
+    Long2DoubleOpenHashMap accumulatedGrad;
+    double weight = 0;
+    List<byte[]> pendingPulls;
+    // private final Map<Integer, Double> weightByModelId = new HashMap<>();
+    // private final Map<Integer, List<byte[]>> pendingPullsByModelId = new HashMap<>();
 
     public ServerNode(
             double alpha,
@@ -77,15 +75,16 @@ public class ServerNode extends AbstractStreamOperator<Tuple2<Integer, byte[]>>
         this.lambda2 = lambda2;
         this.modelOutputTag = modelOutputTag;
         this.numWorkers = numWorkers;
-        this.modelData = new HashMap<>();
         this.sync = sync;
+        this.pendingPulls = new ArrayList<>(numWorkers);
+        this.accumulatedGrad = new Long2DoubleOpenHashMap();
     }
 
     @Override
     public void open() throws Exception {
         super.open();
         psId = getRuntimeContext().getIndexOfThisSubtask();
-        fixedThreadPool = Executors.newFixedThreadPool(numServerCores);
+        // fixedThreadPool = Executors.newFixedThreadPool(numServerCores);
     }
 
     @Override
@@ -99,35 +98,36 @@ public class ServerNode extends AbstractStreamOperator<Tuple2<Integer, byte[]>>
         }
     }
 
-    private Object processPullMessage(byte[] bytesData) {
+    private void processPullMessage(byte[] bytesData) {
         SparsePullModeM sparsePullModeM = MessageUtils.readFromBytes(bytesData, 0);
         Preconditions.checkState(psId == sparsePullModeM.psId);
         int modelId = sparsePullModeM.modelId;
         int workerId = sparsePullModeM.workerId;
         long[] indices = sparsePullModeM.pullModelIndices;
-        double[] pulledValues = modelData.get(modelId).getData(indices);
+        double[] pulledValues = modelData.getData(indices);
         PulledModelM pulledModelM = new PulledModelM(modelId, psId, workerId, pulledValues);
         StreamRecord<Tuple2<Integer, byte[]>> record =
                 new StreamRecord<>(Tuple2.of(workerId, MessageUtils.toBytes(pulledModelM)));
-        synchronized (lock) {
-            output.collect(record);
-        }
-        return new Object();
+        // synchronized (lock) {
+        output.collect(record);
+        // }
+        // return new Object();
     }
 
     private void processPullRpc(byte[] rpc) throws ExecutionException, InterruptedException {
         int modelId = MessageUtils.readModelIdFromSparsePullMessage(rpc, 0);
         if (sync) {
-            List<byte[]> pendingPulls;
-            if (pendingPullsByModelId.containsKey(modelId)) {
-                pendingPulls = pendingPullsByModelId.get(modelId);
-            } else {
-                pendingPulls = new ArrayList<>(numWorkers);
-                pendingPullsByModelId.put(modelId, pendingPulls);
-            }
+            // List<byte[]> pendingPulls;
+            // if (pendingPullsByModelId.containsKey(modelId)) {
+            //    pendingPulls = pendingPullsByModelId.get(modelId);
+            // } else {
+            //    pendingPulls = new ArrayList<>(numWorkers);
+            //    pendingPullsByModelId.put(modelId, pendingPulls);
+            // }
             pendingPulls.add(rpc);
         } else {
-            futuresInEpoch.add(fixedThreadPool.submit(() -> processPullMessage(rpc)));
+            // futuresInEpoch.add(fixedThreadPool.submit(() -> processPullMessage(rpc)));
+            processPullMessage(rpc);
             // processPullMessage(rpc);
             // fixedThreadPool.execute(() -> processPullMessage(rpc));
             // StreamRecord<Tuple2<Integer, byte[]>> pulledMessage = processPullMessage(rpc);
@@ -147,10 +147,10 @@ public class ServerNode extends AbstractStreamOperator<Tuple2<Integer, byte[]>>
             long end = psfZeros.endIndex;
             int modelId = psfZeros.modelId;
             int modelShardSize = (int) (end - start);
-            if (modelData.containsKey(modelId)) {
+            if (modelData != null) {
                 // Already initialized model here.
             } else {
-                modelData.put(modelId, new ServerVector(start, end, new double[modelShardSize]));
+                modelData = new ServerVector(start, end, new double[modelShardSize]);
                 sigma = new double[modelShardSize];
                 z = new double[modelShardSize];
                 n = new double[modelShardSize];
@@ -163,54 +163,52 @@ public class ServerNode extends AbstractStreamOperator<Tuple2<Integer, byte[]>>
         }
     }
 
-    private Object processPushGrad(byte[] pushRpc) {
+    private void processPushGrad(byte[] pushRpc) {
         PushGradM pushGradM = MessageUtils.readFromBytes(pushRpc, 0);
         Preconditions.checkState(pushGradM.psId == psId);
         int modelId = pushGradM.modelId;
         if (sync) {
-            Long2DoubleOpenHashMap tmpGrad;
-            double tmpWeight;
-            if (accumulatedGradByModelId.containsKey(modelId)) {
-                tmpGrad = accumulatedGradByModelId.get(modelId);
-                tmpWeight = weightByModelId.get(modelId);
-            } else {
-                tmpGrad = new Long2DoubleOpenHashMap();
-                tmpWeight = 0;
-                accumulatedGradByModelId.put(modelId, tmpGrad);
-            }
+            Long2DoubleOpenHashMap tmpGrad = accumulatedGrad;
+            // double tmpWeight;
+            // if (accumulatedGradByModelId.containsKey(modelId)) {
+            //    tmpGrad = accumulatedGradByModelId.get(modelId);
+            //    tmpWeight = weightByModelId.get(modelId);
+            // } else {
+            //    tmpGrad = new Long2DoubleOpenHashMap();
+            //    tmpWeight = 0;
+            //    accumulatedGradByModelId.put(modelId, tmpGrad);
+            // }
 
             SparseLongDoubleVector pushedGrad = pushGradM.grad;
-            tmpWeight += pushGradM.weight;
+            weight += pushGradM.weight;
             long[] indices = pushedGrad.indices;
             double[] values = pushedGrad.values;
             for (int i = 0; i < indices.length; i++) {
                 double original = tmpGrad.getOrDefault(indices[i], 0.0);
                 tmpGrad.put(indices[i], original + values[i]);
             }
-            weightByModelId.put(modelId, tmpWeight);
+            // weightByModelId.put(modelId, tmpWeight);
         } else {
-            updateModel(modelId, pushGradM.grad);
+            updateModel(pushGradM.grad);
         }
-        return new Object();
+        // return new Object();
     }
 
     /** Updates the model using accumulated gradient in one iteration. */
-    private void updateModel(int modelId, Long2DoubleOpenHashMap accumulatedGrad) {
-        ServerVector model = modelData.get(modelId);
+    private void updateModel(Long2DoubleOpenHashMap accumulatedGrad) {
         for (Map.Entry<Long, Double> entry : accumulatedGrad.entrySet()) {
-            int index = (int) ((entry.getKey()) - model.startIndex);
+            int index = (int) ((entry.getKey()) - modelData.startIndex);
             double gi = entry.getValue();
-            updateModelOnOneDim(gi, index, model);
+            updateModelOnOneDim(gi, index, modelData);
         }
     }
 
     /** Updates model using one received gradient. */
-    private void updateModel(int modelId, SparseLongDoubleVector grad) {
-        ServerVector model = modelData.get(modelId);
+    private void updateModel(SparseLongDoubleVector grad) {
         for (int i = 0; i < grad.indices.length; i++) {
-            int index = (int) (grad.indices[i] - model.startIndex);
+            int index = (int) (grad.indices[i] - modelData.startIndex);
             double gi = grad.values[i];
-            updateModelOnOneDim(gi, index, model);
+            updateModelOnOneDim(gi, index, modelData);
         }
     }
 
@@ -236,42 +234,43 @@ public class ServerNode extends AbstractStreamOperator<Tuple2<Integer, byte[]>>
         if (sync) {
             // Updates model.
             int modelId = 0;
-            Long2DoubleOpenHashMap grad = accumulatedGradByModelId.remove(modelId);
-            if (grad != null) {
+            if (accumulatedGrad.size() > 0) {
                 // The first iteration contains no pulls.
                 Preconditions.checkState(epochWatermark != 0);
-                updateModel(modelId, grad);
+                updateModel(accumulatedGrad);
+                accumulatedGrad.clear();
             }
-            List<byte[]> pendingPulls = pendingPullsByModelId.remove(modelId);
             if (pendingPulls != null) {
                 // The last iteration contains no pulls.
                 for (byte[] pull : pendingPulls) {
                     // fixedThreadPool.submit(() -> processPullMessage(pull));
                     processPullMessage(pull);
                 }
+                pendingPulls.clear();
             }
-        } else {
-            for (Future<?> future : futuresInEpoch) {
-                future.get();
-            }
-            futuresInEpoch.clear();
         }
+        // else {
+        //    for (Future<?> future : futuresInEpoch) {
+        //        future.get();
+        //    }
+        //    futuresInEpoch.clear();
+        // }
     }
 
     @Override
     public void onIterationTerminated(
             Context context, Collector<Tuple2<Integer, byte[]>> collector) {
-        for (Map.Entry<Integer, ServerVector> model : modelData.entrySet()) {
-            int modelId = model.getKey();
-            ServerVector serverVector = model.getValue();
-            long startIndex = serverVector.startIndex;
-            long endIndex = serverVector.endIndex;
-            double[] data = serverVector.data;
-            Tuple4<Integer, Long, Long, double[]> tuple4 =
-                    Tuple4.of(modelId, startIndex, endIndex, data);
-            output.collect(modelOutputTag, new StreamRecord<>(tuple4));
-        }
+        // for (Map.Entry<Integer, ServerVector> model : modelData.entrySet()) {
+        int modelId = 0;
+        ServerVector serverVector = modelData;
+        long startIndex = serverVector.startIndex;
+        long endIndex = serverVector.endIndex;
+        double[] data = serverVector.data;
+        Tuple4<Integer, Long, Long, double[]> tuple4 =
+                Tuple4.of(modelId, startIndex, endIndex, data);
+        output.collect(modelOutputTag, new StreamRecord<>(tuple4));
+        // }
     }
 
-    private static class SerializableObject implements Serializable {}
+    // private static class SerializableObject implements Serializable {}
 }
